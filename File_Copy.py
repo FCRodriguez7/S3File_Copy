8d4ec44b94efa8b07afc2550c4bee0bc6bc5c60a
FCRodriguez7 <41123693+FCRodriguez7@users.noreply.github.com>
2020-04-07 19:21:47 -0400

    S3 File Copy

---
+++ File_Copy.py
@@ -0,0 +1,37 @@
+import csv
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+ACCESS_KEY = 'AKIATLAFMMWSYHG2TSM4'
+SECRET_KEY = 'Tn5Y/cK1E9R8BGJ6znMdTmG5rZ+T9xnojLu9IV75'
+#MOUNT_PREFIX = '/Volumes/home'
+#AWS_PREFIX = 'originals'
+#BUCKET_NAME = 'yale-image-samples'
+
+def upload_to_aws(file_path_name):
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+    bucket = 'yale-image-samples'
+#    local_file = '/Volumes/home'+file_path_name
+#    s3_file = 'originals'+file_path_name
+    local_file = '/Volumes/home' + file_path_name
+    s3_file = 'originals/'+oid_number+file_extension
+
+    try:
+        s3.upload_file(local_file, bucket, s3_file)
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+
+with open('/Users/fcrodriguez/PycharmProjects/File_Copy/oidpath32320_2Test.csv', newline='') as csv_file:
+    file_extension = '.tif'
+    csv_reader = csv.reader(csv_file)
+    for csv_row in csv_reader:
+        oid_number = csv_row[0]
+        file_path_name = csv_row[1]
+        uploaded = upload_to_aws(file_path_name.lstrip())
